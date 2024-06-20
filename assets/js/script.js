4705d50f492cb45590dbe4cf235e427edcc47326
Ken Cox <159484600+Damitwhy@users.noreply.github.com>
2024-06-21 00:13:39 +0100

    Added display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,35 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame(){
+/** 
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+*/
+function runGame(gameType){
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else if(gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
+    } else if(gameType === "subtract"){
+        displaySubtractQuestion(num1, num2);
+    } else if(gameType === "division"){
+        displayDivisionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -36,7 +57,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
