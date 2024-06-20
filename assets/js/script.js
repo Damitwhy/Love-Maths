02748a7d4d55d609b4edf3f950d3bd701b353ebc
Ken Cox <159484600+Damitwhy@users.noreply.github.com>
2024-06-20 23:44:39 +0100

    Setup structure of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-console.log('Hello from script.js');
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+// function displayDivideQuestion(){
+
+// }
+
