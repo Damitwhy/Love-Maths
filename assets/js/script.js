64b807726fe62939633ee8d7fa3a0fd8a5eae394
Ken Cox <159484600+Damitwhy@users.noreply.github.com>
2024-06-21 04:25:07 +0100

    added fix to division function

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,6 +145,8 @@ function displayMultiplyQuestion(operand1, operand2){
 
 function displayDivisionQuestion(operand1, operand2){
 
+    operand1 = operand1 * operand2;
+
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "/";
